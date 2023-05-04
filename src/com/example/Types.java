a8fe2b54d8d56782a87a994bad5e8c447f5df163
marcosestebandev <marcosestebangodoy@hotmail.com>
2023-05-03 21:19:57 -0300

    Java first commit

---
+++ src/com/example/Types.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class Types {
+    public static void main(String[] args){
+        byte var1 = 1;
+        short var2 = 22;
+        int var3 = 33;
+        long var4 = 111;
+
+        float dec1 = 53.5f;
+        double dec2 = 55.12d;
+
+
+        boolean var5 = true;
+
+        char str1 = 'a';
+        String str2 = "Hola pues";
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(dec1);
+        System.out.println(dec2);
+        System.out.println(var5);
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
